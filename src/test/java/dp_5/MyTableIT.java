bf4dc7b2af88f3e11a72ef8c85c43bd16005dcd3
Saba <nigistsaba@hotmail.com>
2017-05-24 00:53:14 +0200

    adds IT for myTable

---
+++ src/test/java/dp_5/MyTableIT.java
@@ -0,0 +1,16 @@
+package dp_5;
+
+import org.junit.Test;
+
+/**
+ * Created by saba on 24.05.17.
+ */
+public class MyTableIT {
+
+
+    @Test
+    public void createAndShowTable() throws Exception {
+
+    }
+
+}
