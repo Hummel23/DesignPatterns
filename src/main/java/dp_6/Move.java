add8b91ffc920d24590a8242033684a7a10e19fe
Saba <nigistsaba@hotmail.com>
2017-06-06 23:58:26 +0200

    refactoring

--- src/main/java/dp_6/Move.java
+++ src/main/java/dp_6/Move.java
@@ -1,38 +1,38 @@
 package dp_6;
 
-public class Move {
+class Move {
 
     private Point startPoint;
     private Point endPoint;
 
 
-    public Move(Point startPoint, Point endPoint) {
+    Move(Point startPoint, Point endPoint) {
         this.startPoint = startPoint;
         this.endPoint = endPoint;
     }
 
-    public int getStartPointXValue() {
+    int getStartPointXValue() {
         return startPoint.getX();
     }
 
-    public int getStartPointYValue() {
+    int getStartPointYValue() {
         return startPoint.getY();
     }
 
-    public int getEndPointXValue() {
+    int getEndPointXValue() {
         return endPoint.getX();
     }
 
-    public int getEndPointYValue() {
+    int getEndPointYValue() {
         return endPoint.getY();
     }
 
-    public boolean endPointIsWithinDistanceOfStartPoint() {
+    boolean endPointIsWithinDistanceOfStartPoint() {
         return (((startPoint.getX() == endPoint.getX() + 2 || startPoint.getX() == endPoint.getX() -2) && startPoint.getY() == endPoint.getY()) ||
                 (startPoint.getX() == endPoint.getX() && (startPoint.getY() == endPoint.getY() + 2 || startPoint.getY() == endPoint.getY() -2)));
     }
 
-    public int getMiddlePointXValue() {
+    int getMiddlePointXValue() {
         if (startPoint.getX() == endPoint.getX()) {
             return startPoint.getX();
         } else {
@@ -44,7 +44,7 @@ public class Move {
         }
     }
 
-    public int getMiddlePointYValue() {
+    int getMiddlePointYValue() {
         if (startPoint.getY() == endPoint.getY()) {
             return startPoint.getY();
         }else{
